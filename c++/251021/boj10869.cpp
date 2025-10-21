0ecbd91cfa1d2faad70bdbb965780a495b32b05a
Nancy <mandubong1206@gmail.com>
2025-10-21 15:45:06 +0900

    solved / BOJ10869 : 사칙연산 (B5)

---
+++ c++/251021/boj10869.cpp
@@ -0,0 +1,14 @@
+// BOJ10869 : 사칙연산 (B5)
+
+#include <iostream>
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a+b << '\n';
+  std::cout << a-b << '\n';
+  std::cout << a*b << '\n';
+  std::cout << a/b << '\n';
+  std::cout << a%b << '\n';
+  return 0;
+}
